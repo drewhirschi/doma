ca066fd27641b0c47e8a03701e06aa01bbd03de9
Drew Hirschi <drew@atlasai.tools>
2024-02-12 17:45:55 -0800

    fixed import contracts async bug.

--- app/portal/projects/[projectId]/tabs/overview/ImportModal/AddContractsModal.actions.ts
+++ app/portal/projects/[projectId]/tabs/overview/ImportModal/AddContractsModal.actions.ts
@@ -2,12 +2,13 @@
 
 import { revalidatePath } from "next/cache"
 import { serverActionClient } from "@/supabase/ServerClients"
+import { sleep } from "@/utils"
 import { unzipTenantFile } from "@/supabase/StorageServerFunctions"
 
 export async function unzipFile(filepath: string, project_id: string) {
     const supabase = serverActionClient()
 
-     await unzipTenantFile(supabase, filepath)
+     await unzipTenantFile(supabase, filepath) 
      revalidatePath(`/portal/projects/${project_id}`)
     
 }
