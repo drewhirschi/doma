c1bd5dff21144542a984d6cf6da32ae9742f36d2
Andrew Hirschi <drew@atlasai.tools>
2024-03-14 14:00:47 -1000

    allow summary for unimplemented

--- src/components/FormattedInfoViews/Generic.tsx
+++ src/components/FormattedInfoViews/Generic.tsx
@@ -1,24 +1,48 @@
-import { Anchor, Badge, Group, Stack, Text, Title } from "@mantine/core"
+import { Anchor, Badge, Button, Group, Stack, Text, Textarea, Title } from "@mantine/core"
 import { FormatterViewProps, IFormatResponse, IPOwnershipFormatResponse } from "@/types/formattersTypes"
 
 import { EIReferenceLinks } from "./EIReferences"
 import { FormattedInfoWithEiId } from "@/types/complex"
+import { notifications } from "@mantine/notifications"
+import { useForm } from "@mantine/form"
+import { useState } from "react"
 
-export function FormattedGeneric({ info }: FormatterViewProps) {
+export function FormattedGeneric({ info, handleSave }: FormatterViewProps) {
 
-    
+
 
     const data: IFormatResponse = info?.data as unknown as IFormatResponse
 
     const extractedInfoRefs = info?.extracted_information?.map(ei => ei.id) ?? []
-
+    const form = useForm({
+        initialValues: data,
+    });
+    const [loading, setLoading] = useState(false);
 
     return (
-        <Stack>
+        <form onSubmit={form.onSubmit(async (values) => {
+            setLoading(true)
+            try {
 
-            <Text style={{whiteSpace:"pre-wrap"}}>{data?.summary}</Text>
-            <EIReferenceLinks ids={extractedInfoRefs} />
+                await handleSave(values)
+                form.resetDirty()
+            } catch (e) {
+                console.error(e)
+                notifications.show({
+                    title: "Failed to save",
+                    message: "An error occurred while saving the data",
+                    color: "red"
+                })
+            }
+            setLoading(false)
+        })}>
+            <Stack gap={0}>
 
-        </Stack>
+                <Textarea autosize style={{ whiteSpace: "pre-wrap" }} />
+                {extractedInfoRefs.length > 0 && <EIReferenceLinks ids={extractedInfoRefs} />}
+                <Button loading={loading} size="xs" style={{alignSelf: "flex-end"}} mt={"sm"} type="submit" disabled={!form.isDirty()} color="blue">Save</Button>
+
+            </Stack>
+        </form>
     )
 }
