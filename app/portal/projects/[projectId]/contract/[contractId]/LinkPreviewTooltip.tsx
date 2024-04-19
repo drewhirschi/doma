78f4751374deeba9d194a281e8313cdb86090eb0
Drew Hirschi <drew@atlasai.tools>
2024-04-19 13:41:28 -0700

    package clean up

--- app/portal/projects/[projectId]/contract/[contractId]/LinkPreviewTooltip.tsx
+++ app/portal/projects/[projectId]/contract/[contractId]/LinkPreviewTooltip.tsx
@@ -1,56 +1,60 @@
-import { ActionIcon, Group, MantineProvider, Paper } from "@mantine/core";
-import { IconCopy, IconTrash, IconViewfinder } from "@tabler/icons-react";
+// import { ActionIcon, Group, MantineProvider, Paper } from "@mantine/core";
+// import { IconCopy, IconTrash, IconViewfinder } from "@tabler/icons-react";
 
-import { Editor } from "@tiptap/core";
-import ReactDOM from "react-dom";
-import { Tooltip } from "@docs.plus/extension-hyperlink";
-import { createRoot } from "react-dom/client";
-import { useRouter } from "next/navigation";
+// import { Editor } from "@tiptap/core";
+// import ReactDOM from "react-dom";
+// import { createRoot } from "react-dom/client";
+// import { useRouter } from "next/navigation";
 
-export type THyperlinkPreviewModalOptions = {
-    editor: Editor;
-    validate?: (url: string) => boolean;
-    link: HTMLAnchorElement;
-    node?: any;
-    nodePos: number;
-    tippy: Tooltip;
-};
-
-export function linkPreviewToolTip(options: THyperlinkPreviewModalOptions): HTMLElement {
-    const href = options.link.href;
+// // import { Tooltip } from "@docs.plus/extension-hyperlink";
 
 
-    const hyperlinkModal = document.createElement("div");
-    const root = createRoot(hyperlinkModal);
-    root.render(<MantineProvider>
-        <Paper shadow="lg" p={"sm"}>
-            <Group>
-                <ActionIcon variant="default" onClick={() => {
-                    window.history.replaceState(null, '', href);
-                    window.dispatchEvent(new HashChangeEvent('hashchange'));
 
-                    options.tippy.hide();
-                }}>
+// export type THyperlinkPreviewModalOptions = {
+//     editor: Editor;
+//     validate?: (url: string) => boolean;
+//     link: HTMLAnchorElement;
+//     node?: any;
+//     nodePos: number;
+//     tippy: Tooltip;
+// };
 
-                    <IconViewfinder />
-                </ActionIcon>
-                <ActionIcon variant="default" onClick={() => {
-                    navigator.clipboard.writeText(href);
-                    options.tippy.hide();
-                }}>
+// export function linkPreviewToolTip(options: THyperlinkPreviewModalOptions): HTMLElement {
+//     // const href = options.link.href;
 
-                    <IconCopy />
-                </ActionIcon>
-                <ActionIcon variant="default" onClick={() => {
-                    // options.editor.chain().focus().unsetHyerlink().run()
-                    // options.tippy.hide();
-                }}>
 
-                    <IconTrash />
-                </ActionIcon>
-            </Group>
-        </Paper>
-    </MantineProvider>)
+//     // const hyperlinkModal = document.createElement("div");
+//     // const root = createRoot(hyperlinkModal);
+//     // root.render(<MantineProvider>
+//     //     <Paper shadow="lg" p={"sm"}>
+//     //         <Group>
+//     //             <ActionIcon variant="default" onClick={() => {
+//     //                 window.history.replaceState(null, '', href);
+//     //                 window.dispatchEvent(new HashChangeEvent('hashchange'));
 
-    return hyperlinkModal;
-}
+//     //                 options.tippy.hide();
+//     //             }}>
+
+//     //                 <IconViewfinder />
+//     //             </ActionIcon>
+//     //             <ActionIcon variant="default" onClick={() => {
+//     //                 navigator.clipboard.writeText(href);
+//     //                 options.tippy.hide();
+//     //             }}>
+
+//     //                 <IconCopy />
+//     //             </ActionIcon>
+//     //             <ActionIcon variant="default" onClick={() => {
+//     //                 // options.editor.chain().focus().unsetHyerlink().run()
+//     //                 // options.tippy.hide();
+//     //             }}>
+
+//     //                 <IconTrash />
+//     //             </ActionIcon>
+//     //         </Group>
+//     //     </Paper>
+//     // </MantineProvider>)
+
+//     // return hyperlinkModal;
+//     return <></>
+// }
