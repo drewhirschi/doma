5ecef5ab148083f4e337f46d2f72a5fc19d824e0
Drew Hirschi <drew@atlasai.tools>
2024-04-25 16:45:30 -0700

    form fix

--- src/components/FormattedInfoViews/AgreementInfo.tsx
+++ src/components/FormattedInfoViews/AgreementInfo.tsx
@@ -5,40 +5,53 @@ import { DatePickerInput } from "@mantine/dates";
 import { ViewProps } from "./FormattedItemSingle";
 
 export function FormattedAgreementInfo({ form, onChange }: ViewProps<AgreementInfoFormatResponse>) {
-    
+
 
 
 
     return (
-     
-                <Stack gap={4}>
-                    <Textarea 
-                    autosize
-                     {...form.getInputProps('summary')} 
-                     value={form.getInputProps(`summary`).value ?? ""}
 
-                     />
+        <Stack gap={4}>
+            <Textarea
+                autosize
+                {...form.getInputProps('summary')}
+                value={form.getInputProps(`summary`).value ?? ""}
 
-                    <SimpleGrid cols={2} spacing="md">
-                        <TextInput label="Title" {...form.getInputProps('title')} />
-                        <DatePickerInput
-                            firstDayOfWeek={0}
-                            label="Effective date"
-                            placeholder="No date"
-                            clearable
-                            {...form.getInputProps('effective_date')}
-                            value={form.values.effective_date ? new Date(form.values.effective_date) : null}
-                            onChange={(value) => {
-                                form.setFieldValue('effective_date', value)
-                                onChange()
-                            }}
-                        />
-                        <TextInput label="Target" {...form.getInputProps('target_entity')} />
-                        <TextInput label="Counterparty" {...form.getInputProps("counter_party")} />
-                    </SimpleGrid>
+            />
 
-                </Stack>
-         
+            <SimpleGrid cols={2} spacing="md">
+                <TextInput
+                    label="Title"
+                    {...form.getInputProps('title')}
+                    value={form.getInputProps(`title`).value ?? ""}
+
+                />
+                <DatePickerInput
+                    firstDayOfWeek={0}
+                    label="Effective date"
+                    placeholder="No date"
+                    clearable
+                    {...form.getInputProps('effective_date')}
+                    value={form.values.effective_date ? new Date(form.values.effective_date) : null}
+                    onChange={(value) => {
+                        form.setFieldValue('effective_date', value)
+                        onChange()
+                    }}
+                />
+                <TextInput
+                    label="Target"
+                    {...form.getInputProps('target_entity')}
+                    value={form.getInputProps(`target_entity`).value ?? ""}
+                />
+                <TextInput
+                    label="Counterparty"
+                    {...form.getInputProps("counter_party")}
+                    value={form.getInputProps(`counter_party`).value ?? ""}
+                />
+            </SimpleGrid>
+
+        </Stack>
+
 
     )
 }
