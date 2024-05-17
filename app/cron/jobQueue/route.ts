186f5cd758a5698b34ab5e4dc587156039216428
Drew Hirschi <drew@atlasai.tools>
2024-05-16 17:26:59 -0700

    init job queue

---
+++ app/cron/jobQueue/route.ts
@@ -0,0 +1,25 @@
+import Zuva, { checkExtractionJobs } from '@/zuva';
+import { fullAccessServiceClient, routeClient } from '@/supabase/ServerClients';
+
+import type { NextRequest } from 'next/server';
+
+export function GET(request: NextRequest) {
+  const authHeader = request.headers.get('authorization');
+  if (process.env.NODE_ENV !== 'development' && authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
+
+
+  const supabase = fullAccessServiceClient()
+
+  try {
+    
+    return Response.json({}, { status: 200 });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+
+
+}
