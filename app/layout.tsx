bc5057e06ebc321c855327af9a55ad1184244627
Drew Hirschi <drew@atlasai.tools>
2024-08-09 16:34:43 -0700

    removing bad dep

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "@mantine/core/styles.css";
 import '@mantine/notifications/styles.css';
 import '@mantine/dates/styles.css';
-import '@mantine/carousel/styles.css';
 import "./global.css";
 
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
