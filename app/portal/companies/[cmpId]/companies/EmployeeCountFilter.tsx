6bb77ea98f464b04128fecf681a3ced8aa3e6820
Drew Hirschi <drew@atlasai.tools>
2024-11-07 14:47:23 -0800

    bug fixes

--- app/portal/companies/[cmpId]/companies/EmployeeCountFilter.tsx
+++ app/portal/companies/[cmpId]/companies/EmployeeCountFilter.tsx
@@ -32,16 +32,13 @@ export function EmployeeCountFilter() {
         onChange={setEmployees}
         data={[
           //   { value: "none", label: "All Employee counts" },
-          { value: "1-10", label: "1-10" },
-          { value: "11-20", label: "11-20" },
-          { value: "21-50", label: "21-50" },
-          { value: "51-100", label: "51-100" },
-          { value: "101-200", label: "101-200" },
-          { value: "201-500", label: "201-500" },
-          { value: "501-1000", label: "501-1000" },
-          { value: "1001-2000", label: "1001-2000" },
-          { value: "2001-5000", label: "2001-5000" },
-          { value: "5001-10000", label: "5001-10000" },
+          { value: "2 - 10", label: "2-10" },
+          { value: "11 - 50", label: "11-50" },
+          { value: "51 - 200", label: "51-200" },
+          { value: "201 - 500", label: "201-500" },
+          { value: "501 - 1000", label: "501-1000" },
+          { value: "1001 - 5000", label: "1001-5000" },
+          { value: "5001 - 10000", label: "5001-10000" },
           { value: "10001+", label: "10001+" },
         ]}
       />
